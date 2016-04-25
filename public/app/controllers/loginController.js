26ffb96f5b2e2c13df4f62eb56aa6c75f228f856
fufu7755 <fufu7755@gmail.com>
2016-04-25 09:14:52 +0800

    4-25 2

--- public/app/controllers/loginController.js
+++ public/app/controllers/loginController.js
@@ -1,12 +1,18 @@
 ﻿angular
-  .module('hshs').controller('loginController', ['$rootScope', '$scope', 'userService', function ($rootScope, $scope, userService) {
+    .module('hshs').controller('loginController', ['$rootScope', '$scope', 'userService', function ($rootScope, $scope, userService) {
+        $scope.login = function () {
+            userService.login($scope.user);
+        };
 
-      $scope.login = function () {
-          userService.login($scope.user);
-      };
-
-      $scope.logout = function () {
-        userService.logout($rootScope.currentUser.id);
-      };
-
-  }]);
+        $scope.logout = function () {
+            userService.logout($rootScope.currentUser.id);
+        };
+        userService.getClubs().then(function (data) {
+            $scope.clubs = data.data.result;
+            console.log($scope.clubs);
+            $scope.register = function () {
+                userService.register($scope.user);
+                console.log($scope.user);
+            };
+        });
+    }]);
--- public/app/services/userService.js
+++ public/app/services/userService.js
@@ -1,23 +1,23 @@
 ﻿angular
-  .module('hshs').factory('userService', ['$rootScope', '$http', '$location', 'toaster', function ($rootScope, $http, $location, toaster) {
-      var baseUrl = 'http://111.198.74.14:9190/user/';
+  .module('hshs').factory('userService', ['$rootScope', '$http', '$location', 'toaster', 'baseUrl', function ($rootScope, $http, $location, toaster, baseUrl) {
+
       var userService = {};
 
       output = {
           login: function(user) {
               promise = $http({
                   method: 'POST',
-                  url: baseUrl + 'login',
-                  params: { account: user.account, password: user.password },
+                  url: baseUrl + 'vgo/login',
+                  params: { mobile: user.account, password: user.password },
               }).success(function (response) {
-                  if (response.code === 0) {
+                  if (response.statusCode === 0) {
                     toaster.pop('success', '您已成功登录');
                     $location.path('/');
                     $rootScope.currentUser = response.data;
                     console.log($rootScope.currentUser);
                     $('#ModalLogin').modal('hide');
                   } else {
-                    toaster.pop('warning', response.msg);
+                    toaster.pop('warning', response.message);
                   }
               }).error(function (data, status) {
                   toaster.pop('warning', '请稍后再试');
@@ -43,24 +43,42 @@
             return promise;
           },
           register: function(user) {
+              console.log(user)
+              var birthday = user.year + '-' + user.month + '-' + user.day;
             promise =  $http({
               method: 'POST',
-              url: baseUrl + 'regist',
+              url: baseUrl + 'vgo/apply',
               params: {
-                name: user.name,
-                phone: user.phone,
-                captcha: user.captcha,
-                pwd: user.password,
-                repwd: user.repassword
+                  nickName:user.nickName,
+                  name: user.name,
+                  mobile: user.mobile,
+                  code: user.code,
+                  password: user.password,
+                  certificateType: user.certificateType,
+                  certificateNumber: user.certificateNumber,
+                  avatar:user.avatar,
+                  coolShow:user.coolShow,
+                  gender:user.gender,
+                  clubId:140,
+                  birthday:birthday,
+                  email: user.email,
+                  bloodType: user.bloodType,
+                  jacketSize: user.jacketSize,
+                  trousersSize: user.trousersSize,
+                  shoesSize: user.shoesSize,
+                  address: user.address,
+                  postCode: user.postCode,
+                  diseaseHistory: user.diseaseHistory
               }
             }).success(function (response) {
-              if (response.code === 0) {
+              if (response.statusCode === 0) {
                 toaster.pop('success', '您已成功注册');
                 $location.path('/');
                 $('#ModalSignup').modal('hide');
                 $('#ModalLogin').modal('show');
               } else {
-                toaster.pop('warning', response.msg);
+                toaster.pop('warning', response.message);
+
               }
             }).error(function (data, status) {
               toaster.pop('warning', '请稍后再试');
@@ -70,21 +88,33 @@
           getCaptcha: function(user) {
             promise =  $http({
               method: 'POST',
-              url: baseUrl + 'captcha',
+              url: baseUrl + 'vgo/code',
               params: {
-                phone: user.phone
+                  mobile: user.mobile
               }
             }).success(function (response) {
-              if (response.code === 0) {
+              if (response.statusCode === 0) {
                 console.log(response);
                 toaster.pop('success', '注册码已成功发送');
               } else {
-                toaster.pop('warning', response.msg);
+                toaster.pop('warning', response.message);
               }
             }).error(function (data, status) {
               toaster.pop('warning', '请稍后再试');
             });
             return promise;
+          },
+          getClubs: function () {
+              promise = $http({
+                  method: 'GET',
+                  url: baseUrl + 'club/list',
+                  params: {}
+              }).success(function (response) {
+                  return response.result;
+              }).error(function (data, status) {
+                  console.log(status);
+              });
+              return promise;
           }
         };
 
